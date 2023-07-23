62556dff0510cdb55e44c9d308969961e2a88f8f
Pushpendra Kumar <pushpendra.gangwar@outlook.com>
2023-07-23 19:27:54 +0530

    Added project files

---
+++ DuPics/DuPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuPicsApp.swift
+//  DuPics
+//
+//  Created by Pushpendra Kumar on 23/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DuPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
